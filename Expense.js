9b5cfb576c201eb985e79adfa5670bbbcfb6fb46
zhao-0512 <s24113116@stu.edu.tw>
2026-06-17 19:48:15 +0800

    Update Expense.js

--- Expense.js
+++ Expense.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  type: { type: String, required: true },     // 收入 或 支出
-  category: { type: String, required: true }, // 旅遊、交通、伙食、其他...
-  item: { type: String, required: true },     // 消費項目
-  amount: { type: Number, required: true },   // 金額
+  type: { type: String, required: true },     
+  category: { type: String, required: true }, 
+  item: { type: String, required: true },   
+  amount: { type: Number, required: true },  
   date: { type: Date, default: Date.now }
 });
 
